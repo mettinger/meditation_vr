5fb63859d58e73e1cf26568f0c22a9e777a5e62f
mettinger <jettinger35@gmail.com>
2018-06-14 15:29:08 -0400

    First commit

---
+++ Assets/changeColor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+
+public class changeColor : MonoBehaviour
+{
+
+    Renderer thisRend;
+    double i;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        thisRend = GetComponent<Renderer>();
+        i = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector4 oldColor = thisRend.material.color;
+        //Color color = new Color((oldColor.x + (float) 0.01) % 1, oldColor.y, oldColor.z, oldColor.w);
+        i = i + .1;
+        Color color = new Color((float)Math.Sin(i), oldColor.y, oldColor.z, oldColor.w);
+        thisRend.material.SetColor("_Color", color);
+
+    }
+}
---
+++ Assets/vrButtons.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine.UI;
+
+
+using UnityEngine;
+
+
+public class vrButtons : MonoBehaviour
+{
+    
+    //public TextMesh displayText;
+
+    public GameObject spherePrefab;
+    static private int rows = 30;
+    static private int cols = 30;
+    private float zSphereArray = 100;
+    private GameObject[,] newSphere = new GameObject[rows,cols];
+
+    // Use this for initialization
+    void Start()
+    {
+        /*
+        for (int i = 0; i < 2; i++)
+        {
+            newSphere[i] = Instantiate(spherePrefab, new Vector3(0.0f + (float)i, 1.0f, 5.0f), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+        }*/
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                newSphere[i,j] = Instantiate(spherePrefab, new Vector3((float)j - (((float)cols - 1.0f)/2.0f), (float)i - (((float)rows - 1.0f) / 2.0f), zSphereArray), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+            }
+           
+        }
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GvrControllerInput.AppButtonDown)
+        {
+            print("Click App button down");
+            //displayText.text = "App button down";
+        }
+        if (GvrControllerInput.AppButtonUp)
+        {
+            print("Click App button up");
+            //displayText.text = "App button up";
+        }
+    }
+
+}
