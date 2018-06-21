2dfca198eee20fdfbee9cab98df3350c4d15e28d
mettinger <jettinger35@gmail.com>
2018-06-21 11:38:56 -0400

    message

--- Assets/vrButtons.cs
+++ Assets/vrButtons.cs
@@ -100,6 +100,15 @@ public class vrButtons : MonoBehaviour
                 print("click button click counter: " + clickClickCounter.ToString());
             }
         }
+
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed primary button.");
+
+        if (Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed secondary button.");
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle click.");
     }
 
 }
